d97c4691f466279487e9e8a9fcbbf7f796a7b159
Jae Kim <9028364@gmail.com>
2022-02-01 19:05:07 +0900

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
 
 import Header from 'components/Header/App';
+import 'aos/dist/aos.css';
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return <Header />;
 }
 
--- src/components/Header/App.tsx
+++ src/components/Header/App.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
+import AOS from 'aos';
 
 const Header = () => {
-  return <h1 className="text-3xl font-bold underline">Header</h1>;
+  React.useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  });
+  return (
+    <header className="py-4 px-8 bg-header">
+      <div className="mb-32">
+        <h4 className="text-3xl text-white font-bold">Muscardinus</h4>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-24 gap-y-16">
+        <div className="text-white" data-aos="fade-right">
+          <h1 className="text-5xl sm:text-6xl font-medium mb-12">
+            <Typewriter
+              options={{
+                strings: ['Hello', '안녕하세요', 'Bonjour', 'Namaste', 'Hola', 'Oi'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h1>
+          <h5 className="text-2xl font-medium">Trying to be a</h5>
+          <h5 className="text-2xl font-medium">["Professional", "Communicative", "Trendy"]</h5>
+          <h5 className="text-2xl font-medium">Software Developer</h5>
+        </div>
+        <div className="text-white" data-aos="fade-left">
+          1
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
